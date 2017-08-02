97d9a99e70c9c967033507d0f71a8a8dcf560e57
royreg <roy630@gmail.com>
2017-08-02 21:38:43 +0300

    Login and register mechanism working , auth Guard added

--- angular2-src/src/app/components/profile/profile.component.ts
+++ angular2-src/src/app/components/profile/profile.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticateService} from '../../services/authenticate.service'
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: Object
 
-  constructor() { }
+  constructor(private auth : AuthenticateService,
+              private router : Router) { }
 
   ngOnInit() {
+    this.auth.getProfile().subscribe(
+      profile=>{
+        console.log(profile.user);
+        this.user=profile.user;
+
+      },
+    err=>{
+      console.log(err);
+    });
   }
 
 }
